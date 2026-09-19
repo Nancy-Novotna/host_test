f5a159eba0bc98f39a796a422ce5317213bdc499
Nancy Novotna <d23974@oa-opava.cz>
2026-09-19 10:14:40 +0200

    displaying db data test

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include_once 'includes/database.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,12 +10,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Host</title>
     <link rel="stylesheet" href="styles.css">
+
 </head>
 
 <body>
     <div>
         <h1>Hello, World!</h1>
         <input type="button" value="Click Me" onclick="window.location.href='pageOne.html'">
+
+        <?php
+        $sql = "SELECT * FROM names;";
+        $results = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($results);
+
+        if ($resultCheck > 0) {
+            while ($row = mysqli_fetch_assoc($results)) {
+                echo $row['name'] . "<br>";
+            }
+        }
+
+        ?>
+
     </div>
 </body>
 
