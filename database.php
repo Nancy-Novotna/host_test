34b940c40a795df77c74ea50e75c9b681a14029e
Nancy Novotna <d23974@oa-opava.cz>
2026-09-21 15:13:21 +0200

    test

--- database.php
+++ database.php
@@ -2,39 +2,39 @@
 
 // Connecting to a DB on Infinityfree - OLD
 
-// $dbHost = "sql307.infinityfree.com";
-// $dbName = "if0_42921101_test";
+$dbHost = "sql307.infinityfree.com";
+$dbName = "if0_42921101_test";
 
-// $dbUsername = "if0_42921101";
-// $dbPassword = "hostWebovky654";
+$dbUsername = "if0_42921101";
+$dbPassword = "hostWebovky654";
 
-// $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
-
-// if (!$conn) {
-//     die("Chyba připojení: " . mysqli_connect_error());
-// }
-
-
-if ($_SERVER['SERVER_NAME'] === 'localhost') {
-    // Lokální databáze
-    $host = 'localhost';
-    $user = 'root';
-    $password = '';
-    $database = 'names';
-}
-if ($_SERVER['SERVER_NAME'] === 'sql307.infinityfree.com') {
-    // InfinityFree
-    $host = 'sql307.infinityfree.com';
-    $user = 'if0_42921101';
-    $password = 'hostWebovky654';
-    $database = 'if0_42921101_test';
-}
-
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
 if (!$conn) {
-    die("Chyba připojení k databázi: " . mysqli_connect_error());
+    die("Chyba připojení: " . mysqli_connect_error());
 }
+
+
+// if ($_SERVER['SERVER_NAME'] === 'localhost') {
+//     // Lokální databáze
+//     $host = 'localhost';
+//     $user = 'root';
+//     $password = '';
+//     $database = 'names';
+// }
+// if ($_SERVER['SERVER_NAME'] === 'sql307.infinityfree.com') {
+//     // InfinityFree
+//     $host = 'sql307.infinityfree.com';
+//     $user = 'if0_42921101';
+//     $password = 'hostWebovky654';
+//     $database = 'if0_42921101_test';
+// }
+
+// $conn = mysqli_connect($host, $user, $password, $database);
+
+// if (!$conn) {
+//     die("Chyba připojení k databázi: " . mysqli_connect_error());
+// }
 
 
 ?>
